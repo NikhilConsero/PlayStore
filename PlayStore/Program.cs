91cf9272ea0eae3c8887a0223c50735b473cb453
NikhilConsero <nikhil.gurudev@conseroglobal.com>
2024-11-26 13:13:32 +0530

    ch4

--- PlayStore/Program.cs
+++ PlayStore/Program.cs
@@ -1,15 +1,20 @@
 ﻿using PlayStore;
-Console.WriteLine("My Play Store");
-//Creating a object for the class
-//new keyword is used to create an object and sapce in heap for memory
-User u = new User();
-u.CreateUser();
-Categories c = new Categories(11, "Fashion", "Fashion test");
-Console.WriteLine(c.Name);
-Console.WriteLine(c.Id);
-Console.WriteLine(c.Description);
-c.AddCategory(3, "ABC");
-c.AddCategory(2, "XYZ", "Test");
-Categories d = c;
-int x;
-int y;
+//Console.WriteLine("My Play Store");
+////Creating a object for the class
+////new keyword is used to create an object and sapce in heap for memory
+//User u = new User();
+//u.CreateUser();
+//Categories c = new Categories(11, "Fashion", "Fashion test");
+//Console.WriteLine(c.Name);
+//Console.WriteLine(c.Id);
+//Console.WriteLine(c.Description);
+//c.AddCategory(3, "ABC");
+//c.AddCategory(2, "XYZ", "Test");
+//Categories d = c;
+int x = 10;
+object o = x; //Boxing 
+string s = o.ToString(); //UnBoxing
+Report report = new Report();
+report.GetReport<int>(12);
+report.GetReport2<int, string>(12, "hello");
+report.GenerateReport();
---
+++ PlayStore/Report.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayStore
+{
+    public class Report
+    {
+        public void GetReport<T>(T reportId)
+        {
+            
+        }
+        public void GetReport2<T1,T2>(T1 reportId1, T2 reportId2)
+        {
+
+
+        }
+        public void GenerateReport() 
+        {
+            string data = "Report1,Report2,Report3";
+            string[] ot=data.Split(",");
+            foreach (string item in ot)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}

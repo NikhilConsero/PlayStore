984944b962af7f160363e56a83b3da51390c8999
NikhilConsero <nikhil.gurudev@conseroglobal.com>
2024-11-26 15:59:09 +0530

    ch6

--- PlayStore/Account.cs
+++ PlayStore/Account.cs
@@ -47,9 +47,39 @@ namespace BankManage
             else
             {
 
-            }
             Console.WriteLine($"You have Withdrawn {amt}");
             this.crntamt = this.crntamt - amt;
+            }
+        }
+    }
+    public class CA : Account
+    {
+        int crntamt;
+        public CA()
+        {
+            Console.Write("Enter the Initial Amount:");
+            this.crntamt = Convert.ToInt32(Console.ReadLine());
+        }
+        public void CalculateInterest(int amt, int time)
+        {
+            int interest = Convert.ToInt32((amt * 0.12 * time) / 100);
+            Console.WriteLine($"Interest is {interest}");
+        }
+
+        public void deposit(int amt)
+        {
+            Console.WriteLine($"You have deposited {amt}");
+            this.crntamt = this.crntamt + amt;
+        }
+
+        public void ViewBalance()
+        {
+            Console.WriteLine($"The Balance is:{this.crntamt}");
+        }
+
+        public void withdraw(int amt)
+        {
+            throw new NotImplementedException();
         }
     }
 }
